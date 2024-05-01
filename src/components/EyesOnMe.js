b08b2e33dd3b13445df3d4ea89380f2e3d3359e7
benard <benardboiwo703@gmail.com>
2024-05-01 08:21:25 +0300

    Event Handling in React Lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,38 @@
 // Code EyesOnMe Component Here
+import React, { useState } from 'react';
+
+const EventHandlingComponent = () => {
+  const [buttonClicked, setButtonClicked] = useState(false);
+
+  const handleClick = () => {
+    setButtonClicked(true);
+  };
+
+  const handleFocus = () => {
+    console.log('Good!');
+  };
+
+  const handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+  };
+
+  return (
+    <div>
+      <input 
+        type="password" 
+        onChange={() => {}} 
+        placeholder="Enter password" 
+      />
+      <button 
+        onClick={handleClick}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+      >
+        Eyes on me
+      </button>
+      {buttonClicked && <p>You clicked the button!</p>}
+    </div>
+  );
+};
+
+export default EventHandlingComponent;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,28 @@
-// Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
-}
+import React, { useState } from 'react';
 
-export default Keypad;
+const EventHandlingComponent = () => {
+  const [buttonClicked, setButtonClicked] = useState(false);
+
+  const handleClick = () => {
+    setButtonClicked(true);
+  };
+
+  const handleInputChange = () => {
+    console.log('Entering password...');
+  };
+
+  return (
+    <div>
+      <input 
+        type="password" 
+        onChange={handleInputChange} 
+        placeholder="Enter password" 
+      />
+      <button onClick={handleClick}>Click Me</button>
+      {buttonClicked && <p>You clicked the button!</p>}
+    </div>
+  );
+};
+
+export default EventHandlingComponent;
